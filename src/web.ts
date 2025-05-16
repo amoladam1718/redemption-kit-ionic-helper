1c5115a3e41ab5c9215fa50ceb9f752b2a8ec7f1
amolkad <amol.kadam@loylty.com>
2025-05-16 15:27:55 +0530

    Your commit message

--- src/web.ts
+++ src/web.ts
@@ -3,7 +3,7 @@ import { WebPlugin } from '@capacitor/core';
 import type { RedemptionKitIonicHelperPlugin } from './definitions';
 
 export class RedemptionKitIonicHelperWeb extends WebPlugin implements RedemptionKitIonicHelperPlugin {
-  async echo(options: { value: string }): Promise<{ value: string }> {
+  async initializeAndLaunch(options: { value: string, name: string }): Promise<{ value: string }> {
     console.log('ECHO', options);
     return options;
   }
