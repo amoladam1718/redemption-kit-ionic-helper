5807f69645369f167abe40b9b9ca63ea631a36bb
amolkad <amol.kadam@loylty.com>
2025-05-16 13:01:19 +0530

    Redemption ket helper

--- android/src/main/java/com/loylty/plugins/elitesdk/RedemptionKitIonicHelperPlugin.java
+++ android/src/main/java/com/loylty/plugins/elitesdk/RedemptionKitIonicHelperPlugin.java
@@ -1,22 +1,49 @@
 package com.loylty.plugins.elitesdk;
 
-import com.getcapacitor.JSObject;
+import android.text.TextUtils;
+import android.util.Log;
+
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
+import com.rewardz.redemptionkit.LRRedemptionKit;
+import com.rewardz.redemptionkit.interfaces.LRRedemptionKitHandler;
 
 @CapacitorPlugin(name = "RedemptionKitIonicHelper")
-public class RedemptionKitIonicHelperPlugin extends Plugin {
-
-    private RedemptionKitIonicHelper implementation = new RedemptionKitIonicHelper();
-
+public class RedemptionKitIonicHelperPlugin extends Plugin implements LRRedemptionKitHandler {
     @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    public void initializeAndLaunch(PluginCall call) {
+        String apiKey = call.getString("apiKey");
+        Log.i("kk", "apiKey............: "+apiKey);
+        String customerId = call.getString("customerId");
+        Log.i("kk", "customerId............: "+customerId);
 
-        JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
-        call.resolve(ret);
+        if (TextUtils.isEmpty(apiKey)) {
+            call.reject("Missing apiKey");
+            return;
+        } else if (TextUtils.isEmpty(customerId)) {
+            call.reject("Missing customerId");
+            return;
+        }
+
+        getActivity().runOnUiThread(() -> LRRedemptionKit.getInstance().initializeAndLaunch(getActivity(),
+                apiKey,
+                RedemptionKitIonicHelperPlugin.this,
+                customerId,
+                0,
+                e -> Log.d("LRRedemptionKit", "LRRedemptionKit Exception.............: " + e.getMessage())));
+
+        call.resolve();
+    }
+
+    @Override
+    public void sdkLoggedOut() {
+
+    }
+
+    @Override
+    public void sdkSessionNotifier(boolean b) {
+
     }
 }
